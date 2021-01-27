3b6c7c5dc3d9cf8a9fa723da02e9269827fe2a5b
deezy <danielshiaolin@gmail.com>
2021-01-28 00:47:06 +1100

    added in components

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -1,44 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './containers/Header';
-import React, {useState} from 'react';
+import React, { Component } from "react";
+import Footer from "./components/Footer/footer";
+import Header from "./components/Header/header";
+import EmployeeTable from "./components/EmployeeTable/EmployeeTable";
+import Wrapper from './components/Wrapper/index'
 
 
-function App(props) {
 
-  console.log(props);
 
-  const [header, setHeader] = useState("Default");
-
-  const [bgColour, setBgColour] =useState('black')
-
-  function changeHeader() {
-    console.log('aaasd');
-    setHeader('ABCDE');
-  }
+function App() {
 
   return (
-    <div className="App">
-      
-      <Header title={header}></Header>
+    <>
 
-      <button onClick={changeHeader}>Change header </button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to .
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Header />
+      <Wrapper>
+        <EmployeeTable />
+      </Wrapper>
+      <Footer />
+
+    </>
+  )
 }
+
 
 export default App;
